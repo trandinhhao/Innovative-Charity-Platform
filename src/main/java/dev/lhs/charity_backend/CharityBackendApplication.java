29bc56fba2d7bb004d100d4c711649ad415a2a57
trandinhhao <haodinhtran06@gmail.com>
2025-07-30 23:29:40 +0700

    create project

---
+++ src/main/java/dev/lhs/charity_backend/CharityBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.lhs.charity_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharityBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/lhs/charity_backend/CharityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lhs.charity_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
